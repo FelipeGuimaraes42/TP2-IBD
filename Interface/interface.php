0075cc2ac0eb6cf31b13e7927539b5f64db4d42d
Felipe Guimarães <felipeguima4@gmail.com>
2019-11-15 21:58:51 -0300

    Correção
    Adição de um arquivo .css
    Remoção de linhas "totais" no brasil.csv

---
+++ Interface/interface.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>TP2 IBD</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <h1>Queimadas no Brasil</h1>
+        <div>
+            <button>
+                <a href="total_focos_estado.php">Total Focos de Incêndios Por Estado</a>
+            </button>
+        </div><br/><br/>
+        <div>
+            <button>
+                <a href="total_focos_ano.php">Total Focos de Incêndios Por Ano</a>
+            </button>
+        </div><br/><br/>
+    </body>
+</html>
