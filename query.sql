efda85c88fafb0855716628be081641602a12b8f
Felipe Guimarães <felipeguima4@gmail.com>
2019-11-17 15:45:21 -0300

    Relacionamento mxn
    Adição do relacionamento mxn: vários estados podem ter vários biomas.
    CSV e queries.

--- query.sql
+++ query.sql
@@ -15,7 +15,7 @@ create table regioes(
     primary key (id_regiao)
 )default charset= utf8;
 
-load data local infile "C:\\wamp64\\www\\TP2-IBD-estado_regiao\\CSVs\\regioes.csv" into table regioes
+load data local infile "C:\\wamp64\\www\\TP2-IBD\\CSVs\\regioes.csv" into table regioes
 fields terminated by ';' lines terminated by '\n' ignore 1 lines (regiao);
 
 select regiao from regioes;
@@ -25,12 +25,12 @@ select * from regioes;
 create table estados(
 	sigla_estado char(2),
     estado varchar(25),
-    id_regiao varchar(15),
+    id_regiao int,
     foreign key (id_regiao) references regioes (id_regiao),
     primary key(sigla_estado)
 )default charset= utf8;
 
-load data local infile "C:\\wamp64\\www\\TP2-IBD-estado_regiao\\CSVs\\estados.csv" into table estados
+load data local infile "C:\\wamp64\\www\\TP2-IBD\\CSVs\\estados.csv" into table estados
 fields terminated by ';' lines terminated by '\n' ignore 1 lines (sigla_estado, estado, id_regiao);
 
 select estado from estados;
@@ -41,19 +41,34 @@ select estado, regiao from regioes natural join estados order by regiao, estado;
 
 -- drop table biomas;
 create table biomas(
-	id integer auto_increment,
+	id_bioma integer auto_increment,
     bioma varchar(15),
-    primary key(id)
+    primary key(id_bioma)
 )default charset= utf8;
 
-load data local infile "C:\\wamp64\\www\\TP2-IBD-estado_regiao\\CSVs\\biomas.csv" into table biomas
+load data local infile "C:\\wamp64\\www\\TP2-IBD\\CSVs\\biomas.csv" into table biomas
 fields terminated by ';' lines terminated by '\n' ignore 1 lines (bioma);
 
 select bioma from biomas;
 select * from biomas;
 
--- drop table incendios_brasil;
+-- drop table biomas_estados;
+create table biomas_estados(
+	sigla_estado char(2),
+    id_bioma int,
+    primary key (sigla_estado, id_bioma),
+    foreign key (sigla_estado) references estados(sigla_estado),
+    foreign key (id_bioma) references biomas(id_bioma)
+)default charset= utf8;
 
+
+load data local infile "C:\\wamp64\\www\\TP2-IBD\\CSVs\\estado_bioma.csv" into table biomas_estados
+fields terminated by ';' lines terminated by '\n' ignore 1 lines (sigla_estado, id_bioma);
+
+select * from biomas natural join biomas_estados natural join estados;
+select estado, bioma from biomas natural join biomas_estados natural join estados;
+
+-- drop table incendios_brasil;
 create table incendios_ano(
 	id integer auto_increment not null,
 	ano integer,
@@ -62,10 +77,9 @@ create table incendios_ano(
     periodo date,
     primary key(id)
 ) default charset= utf8;
-show tables;
 
 -- CAMINHO PARA INCENDIOS_BRASIL.CSV é o diretório no qual está o arquivo incendios_brasil.csv as barras invertidas precisam ser duplas
-load data local infile "C:\\wamp64\\www\\TP2-IBD-estado_regiao\\CSVs\\incendios_ano.csv" into table incendios_ano 
+load data local infile "C:\\wamp64\\www\\TP2-IBD\\CSVs\\incendios_ano.csv" into table incendios_ano 
 fields terminated by ';' lines terminated by '\n' ignore 1 lines
 (ano, mes, numero, @datevar) set periodo= str_to_date(@datevar, '%d/%m/%Y');
 
@@ -88,7 +102,7 @@ create table incendios_estado(
 ) default charset= utf8;
 show tables;
 
-load data local infile "C:\\wamp64\\www\\TP2-IBD-estado_regiao\\CSVs\\incendios_estado.csv" into table incendios_estado 
+load data local infile "C:\\wamp64\\www\\TP2-IBD\\CSVs\\incendios_estado.csv" into table incendios_estado 
 fields terminated by '\t' lines terminated by '\n' ignore 1 lines
 (ano, sigla_estado, mes, numero, @datevar) set periodo= str_to_date(@datevar, '%d/%m/%Y');
 
@@ -98,12 +112,3 @@ select sigla_estado, sum(numero) as soma from incendios_estado group by sigla_es
 
 -- Consulta com Join
 select estado, sum(numero) as soma from incendios_estado natural join estados group by sigla_estado order by soma desc;
-
-
-
-
-
-
-
-
-
